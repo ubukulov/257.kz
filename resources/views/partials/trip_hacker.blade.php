8ef79f82ed4c371bcfb9fbb32d3330aad905c430
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2019-12-11 12:16:12 +0600

    fix

--- resources/views/partials/trip_hacker.blade.php
+++ resources/views/partials/trip_hacker.blade.php
@@ -1,3 +1,3 @@
 <div class="trip_hacker">
-    <iframe src="http://triphacker.kz/widget" border="0" style="width: 100%; border:0; height: 46px; background: #F8FAFC;"></iframe>
+    <iframe src="https://triphacker.kz/widget" border="0" style="width: 100%; border:0; height: 46px; background: #F8FAFC;"></iframe>
 </div>
