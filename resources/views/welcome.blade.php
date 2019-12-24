7a30303103e47d4b7bba20b2afd04e9f5379861f
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2019-12-24 12:03:17 +0600

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,9 @@
 
     @include('partials.trip_hacker')
 
-    <hot-component :tour-cities="{{ json_encode($arr_cities) }}" :currency="{{ $currency }}" :selected-currency="{{ $selected_currency }}" :currencies="{{ json_encode($currencies) }}"></hot-component>
+    {{--<hot-component :tour-cities="{{ json_encode($arr_cities) }}" :currency="{{ $currency }}" :selected-currency="{{ $selected_currency }}" :currencies="{{ json_encode($currencies) }}"></hot-component>--}}
 
-    <iframe src="https://triphacker.kz/hot_tour/widget?company_id=7" border="0" width="100%" height="500px" style="border:0"></iframe>
+    <iframe src="https://triphacker.kz/hot_tour/widget?company_id=7" border="0" width="100%" height="650px" style="border:0"></iframe>
 
     @include('partials.our_advantages')
     @include('partials.our_partners')
